9035cea27ed5f83adf985850d1629abe26f216ed
mrcalzon02 <mrcalzon02@yahoo.com>
2026-05-29 14:08:05 -0800

    Architecture: add control profile title helper to layer h

--- src/mechanist/LayerH.java
+++ src/mechanist/LayerH.java
@@ -2,4 +2,14 @@ package mechanist;
 
 public class LayerH {
     public LayerH() {}
+
+    static String controlProfileTitle(GamePanel panel) {
+        switch (panel.controlsTab) {
+            case 1: return "XBOX CONTROL REFERENCE";
+            case 2: return "PLAYSTATION CONTROL REFERENCE";
+            case 3: return "STEAM DECK CONTROL REFERENCE";
+            case 4: return "GENERIC CONTROLLER REFERENCE";
+            default: return "KEYBOARD AND MOUSE CONTROL REFERENCE";
+        }
+    }
 }
