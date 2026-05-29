63b340adef7804f06020a434a8a65925369888b8
mrcalzon02 <mrcalzon02@yahoo.com>
2026-05-29 13:16:14 -0800

    Architecture: add quality of life helper to layer e

--- src/mechanist/LayerE.java
+++ src/mechanist/LayerE.java
@@ -2,4 +2,10 @@ package mechanist;
 
 public class LayerE {
     public LayerE() {}
+
+    static void applyQoL(GamePanel panel, String message) {
+        panel.logEvent(message);
+        DebugLog.audit("GAMEPLAY_QOL_OPTIONS", GameplayQualityOfLifeAuthority.auditSummary(panel.options));
+        panel.repaint();
+    }
 }
