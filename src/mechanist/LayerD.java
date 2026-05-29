b4130a1c99c77bebaac6ac6147624fdf5089f81f
mrcalzon02 <mrcalzon02@yahoo.com>
2026-05-29 12:42:22 -0800

    Architecture: add dropdown toggle to layer d

--- src/mechanist/LayerD.java
+++ src/mechanist/LayerD.java
@@ -2,4 +2,10 @@ package mechanist;
 
 public class LayerD {
     public LayerD() {}
+
+    static void toggleGraphicsDropdown(GamePanel panel, int which) {
+        panel.graphicsDropdown = (panel.graphicsDropdown == which) ? -1 : which;
+        panel.logEvent("Graphics dropdown " + (panel.graphicsDropdown < 0 ? "closed" : "opened") + ".");
+        panel.repaint();
+    }
 }
