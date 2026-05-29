da6a7c33a5e795da23896338e7ed9c2117b1d0ca
mrcalzon02 <mrcalzon02@yahoo.com>
2026-05-29 12:20:27 -0800

    Architecture: fill layer b with runtime volume controls

--- src/mechanist/LayerB.java
+++ src/mechanist/LayerB.java
@@ -4,6 +4,21 @@ public class LayerB {
     public LayerB() {
     }
 
-    public void initialize() {
+    static void changeSfxVolume(GamePanel panel, int delta) {
+        panel.logEvent(OptionsBoundaryAuthority.changeSfxVolume(panel.options, delta));
+        panel.sounds.play("button", panel.options);
+        panel.repaint();
+    }
+
+    static void changeMusicVolume(GamePanel panel, int delta) {
+        panel.logEvent(OptionsBoundaryAuthority.changeMusicVolume(panel.options, delta));
+        panel.sounds.setMusicVolume(panel.options);
+        panel.repaint();
+    }
+
+    static void changeConversationVolume(GamePanel panel, int delta) {
+        panel.logEvent(OptionsBoundaryAuthority.changeConversationVolume(panel.options, delta));
+        panel.sounds.play("type", panel.options);
+        panel.repaint();
     }
 }
