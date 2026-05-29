3146097fed9d5ff633a8ce7e5f688471b2304ce3
mrcalzon02 <mrcalzon02@yahoo.com>
2026-05-29 12:24:35 -0800

    Architecture: fill layer c with lighting option toggle

--- src/mechanist/LayerC.java
+++ src/mechanist/LayerC.java
@@ -2,4 +2,10 @@ package mechanist;
 
 public class LayerC {
     public LayerC() {}
+
+    static void cycleLightingFx(GamePanel panel) {
+        int next = (panel.options.lightingFxIndex + 1) % GameOptions.LIGHTING_FX_LABELS.length;
+        panel.logEvent(OptionsBoundaryAuthority.setLightingFxIndex(panel.options, next));
+        panel.repaint();
+    }
 }
