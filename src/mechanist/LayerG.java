a4a5edcde0dded9c2b8c6092ea1fcb7336e48a95
mrcalzon02 <mrcalzon02@yahoo.com>
2026-05-29 14:04:03 -0800

    Architecture: batch display option helpers into layer g

--- src/mechanist/LayerG.java
+++ src/mechanist/LayerG.java
@@ -2,4 +2,18 @@ package mechanist;
 
 public class LayerG {
     public LayerG() {}
+
+    static void cycleWindowMode(GamePanel panel) {
+        panel.logEvent(OptionsBoundaryAuthority.cycleWindowMode(panel.options));
+        panel.repaint();
+    }
+
+    static void changeResolution(GamePanel panel, int delta) {
+        panel.logEvent(OptionsBoundaryAuthority.changeResolution(panel.options, delta));
+        panel.repaint();
+    }
+
+    static void applyWindowMode(GamePanel panel) {
+        LayerD.applyWindowMode(panel);
+    }
 }
