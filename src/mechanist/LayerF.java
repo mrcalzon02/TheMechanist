3054a1c6ffbf441e7e5fe5e4b3df017bf27a70d1
mrcalzon02 <mrcalzon02@yahoo.com>
2026-05-29 13:30:35 -0800

    Architecture: add diagnostics visibility helper to layer f

--- src/mechanist/LayerF.java
+++ src/mechanist/LayerF.java
@@ -2,4 +2,13 @@ package mechanist;
 
 public class LayerF {
     public LayerF() {}
+
+    static void togglePerformanceDiagnostics(GamePanel panel) {
+        String line = panel.performanceDiagnostics.toggle();
+        panel.options.diagnosticsOverlay = panel.performanceDiagnostics.visible();
+        panel.options.save();
+        panel.logEvent(line);
+        DebugLog.audit("PERFORMANCE_DIAGNOSTICS", panel.performanceDiagnostics.auditSummary());
+        panel.repaint();
+    }
 }
