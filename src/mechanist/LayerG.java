d7567420bc836752747eba95980a829bd3bc3375
mrcalzon02 <mrcalzon02@yahoo.com>
2026-05-29 17:10:19 -0800

    Architecture: add art quality helper to layer g

--- src/mechanist/LayerG.java
+++ src/mechanist/LayerG.java
@@ -16,4 +16,15 @@ public class LayerG {
     static void applyWindowMode(GamePanel panel) {
         LayerD.applyWindowMode(panel);
     }
+
+    static void cycleArtQuality(GamePanel panel) {
+        panel.options.artQualityIndex = Math.floorMod(panel.options.artQualityIndex + 1, GameOptions.ART_QUALITY_LABELS.length);
+        panel.options.applyGeneratedAssetRuntimeProperties();
+        panel.options.save();
+        AssetManager.reloadGeneratedAssetRuntime();
+        panel.images.reloadArtQuality(panel.options);
+        panel.sounds.play("tab", panel.options);
+        panel.logEvent("Art quality cache now " + panel.options.artQualityLabel() + ".");
+        panel.repaint();
+    }
 }
