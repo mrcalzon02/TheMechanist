ad2916c72e6780d22c54a862e7880e3c51898c00
mrcalzon02 <mrcalzon02@yahoo.com>
2026-06-04 00:34:44 -0800

    LayerJ: convert to deprecated JVM runtime shim

--- src/mechanist/LayerJ.java
+++ src/mechanist/LayerJ.java
@@ -1,41 +1,17 @@
 package mechanist;
 
+/** @deprecated Use JvmRuntimeOptionsBridge. */
+@Deprecated
 public class LayerJ {
     public LayerJ() {}
 
-    static void cycleJvmRuntimeProfile(GamePanel panel) {
-        JvmRuntimeOptionsSubsystem.cycleJvmRuntimeProfile(panel);
-    }
-
-    static void cycleJvmGarbageCollector(GamePanel panel) {
-        JvmRuntimeOptionsSubsystem.cycleJvmGarbageCollector(panel);
-    }
-
-    static void cycleJvmPipelineProfile(GamePanel panel) {
-        JvmRuntimeOptionsSubsystem.cycleJvmPipelineProfile(panel);
-    }
-
-    static void changeJvmMemory(GamePanel panel, int deltaMb) {
-        JvmRuntimeOptionsSubsystem.changeJvmMemory(panel, deltaMb);
-    }
-
-    static void toggleJvmStringDeduplication(GamePanel panel) {
-        JvmRuntimeOptionsSubsystem.toggleJvmStringDeduplication(panel);
-    }
-
-    static void toggleJvmTransparentAcceleration(GamePanel panel) {
-        JvmRuntimeOptionsSubsystem.toggleJvmTransparentAcceleration(panel);
-    }
-
-    static void toggleJvmNoAa(GamePanel panel) {
-        JvmRuntimeOptionsSubsystem.toggleJvmNoAa(panel);
-    }
-
-    static void acceptJvmSettingsAndRestart(GamePanel panel) {
-        JvmRuntimeOptionsSubsystem.acceptJvmSettingsAndRestart(panel);
-    }
-
-    static boolean isWindowsHost() {
-        return JvmRuntimeOptionsSubsystem.isWindowsHost();
-    }
+    static void cycleJvmRuntimeProfile(GamePanel panel) { JvmRuntimeOptionsBridge.cycleJvmRuntimeProfile(panel); }
+    static void cycleJvmGarbageCollector(GamePanel panel) { JvmRuntimeOptionsBridge.cycleJvmGarbageCollector(panel); }
+    static void cycleJvmPipelineProfile(GamePanel panel) { JvmRuntimeOptionsBridge.cycleJvmPipelineProfile(panel); }
+    static void changeJvmMemory(GamePanel panel, int deltaMb) { JvmRuntimeOptionsBridge.changeJvmMemory(panel, deltaMb); }
+    static void toggleJvmStringDeduplication(GamePanel panel) { JvmRuntimeOptionsBridge.toggleJvmStringDeduplication(panel); }
+    static void toggleJvmTransparentAcceleration(GamePanel panel) { JvmRuntimeOptionsBridge.toggleJvmTransparentAcceleration(panel); }
+    static void toggleJvmNoAa(GamePanel panel) { JvmRuntimeOptionsBridge.toggleJvmNoAa(panel); }
+    static void acceptJvmSettingsAndRestart(GamePanel panel) { JvmRuntimeOptionsBridge.acceptJvmSettingsAndRestart(panel); }
+    static boolean isWindowsHost() { return JvmRuntimeOptionsBridge.isWindowsHost(); }
 }
