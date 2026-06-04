f623cda484e910efcb04a0eba00cacab4caf678b
mrcalzon02 <mrcalzon02@yahoo.com>
2026-06-04 00:28:40 -0800

    LayerG: convert to deprecated viewport asset shim

--- src/mechanist/LayerG.java
+++ src/mechanist/LayerG.java
@@ -1,52 +1,17 @@
 package mechanist;
 
-import mechanist.assets.AssetManager;
-
+/** @deprecated Use ViewportAssetOptionsRuntime. */
+@Deprecated
 public class LayerG {
     public LayerG() {}
 
-    static void cycleWindowMode(GamePanel panel) {
-        panel.logEvent(OptionsBoundaryAuthority.cycleWindowMode(panel.options));
-        panel.repaint();
-    }
-
-    static void changeResolution(GamePanel panel, int delta) {
-        panel.logEvent(OptionsBoundaryAuthority.changeResolution(panel.options, delta));
-        panel.repaint();
-    }
-
-    static void applyWindowMode(GamePanel panel) {
-        LayerD.applyWindowMode(panel);
-    }
-
-    static void cycleArtQuality(GamePanel panel) {
-        panel.options.artQualityIndex = Math.floorMod(panel.options.artQualityIndex + 1, GameOptions.ART_QUALITY_LABELS.length);
-        panel.options.applyGeneratedAssetRuntimeProperties();
-        panel.options.save();
-        AssetManager.reloadGeneratedAssetRuntime();
-        panel.images.reloadArtQuality(panel.options);
-        panel.sounds.play("tab", panel.options);
-        panel.logEvent("Texture package now " + panel.options.artQualityResolutionLabel() + " (" + panel.options.artQualityFolder() + ").");
-        panel.repaint();
-    }
-
-    static void chooseGeneratedAssetPayloadRoot(GamePanel panel) {
-        GeneratedArtPayloadOptionsSubsystem.chooseGeneratedAssetPayloadRoot(panel);
-    }
-
-    static void clearGeneratedAssetPayloadRoot(GamePanel panel) {
-        GeneratedArtPayloadOptionsSubsystem.clearGeneratedAssetPayloadRoot(panel);
-    }
-
-    static void cycleMapTileSize(GamePanel panel) {
-        MapViewportOptionsSubsystem.cycleMapTileSize(panel);
-    }
-
-    static boolean worldZoomControlActive(GamePanel panel) {
-        return MapViewportOptionsSubsystem.worldZoomControlActive(panel);
-    }
-
-    static void changeWorldZoom(GamePanel panel, int delta, String source) {
-        MapViewportOptionsSubsystem.changeWorldZoom(panel, delta, source);
-    }
+    static void cycleWindowMode(GamePanel panel) { ViewportAssetOptionsRuntime.cycleWindowMode(panel); }
+    static void changeResolution(GamePanel panel, int delta) { ViewportAssetOptionsRuntime.changeResolution(panel, delta); }
+    static void applyWindowMode(GamePanel panel) { ViewportAssetOptionsRuntime.applyWindowMode(panel); }
+    static void cycleArtQuality(GamePanel panel) { ViewportAssetOptionsRuntime.cycleArtQuality(panel); }
+    static void chooseGeneratedAssetPayloadRoot(GamePanel panel) { ViewportAssetOptionsRuntime.chooseGeneratedAssetPayloadRoot(panel); }
+    static void clearGeneratedAssetPayloadRoot(GamePanel panel) { ViewportAssetOptionsRuntime.clearGeneratedAssetPayloadRoot(panel); }
+    static void cycleMapTileSize(GamePanel panel) { ViewportAssetOptionsRuntime.cycleMapTileSize(panel); }
+    static boolean worldZoomControlActive(GamePanel panel) { return ViewportAssetOptionsRuntime.worldZoomControlActive(panel); }
+    static void changeWorldZoom(GamePanel panel, int delta, String source) { ViewportAssetOptionsRuntime.changeWorldZoom(panel, delta, source); }
 }
