6d8c41384ef09ce069db5240f02ac41038f50a57
mrcalzon02 <mrcalzon02@yahoo.com>
2026-06-04 00:18:55 -0800

    LayerC: convert to deprecated display performance shim

--- src/mechanist/LayerC.java
+++ src/mechanist/LayerC.java
@@ -1,39 +1,13 @@
 package mechanist;
 
+/** @deprecated Use DisplayPerformanceOptionsRuntime. */
+@Deprecated
 public class LayerC {
     public LayerC() {}
 
-    static void cycleLightingFx(GamePanel panel) {
-        int next = (panel.options.lightingFxIndex + 1) % GameOptions.LIGHTING_FX_LABELS.length;
-        panel.logEvent(OptionsBoundaryAuthority.setLightingFxIndex(panel.options, next));
-        panel.repaint();
-    }
-
-    static void toggleReducedMotion(GamePanel panel) {
-        panel.logEvent(OptionsBoundaryAuthority.toggleReducedMotion(panel.options));
-        DisplayScaleOptionsSubsystem.applyOptions(panel);
-        panel.repaint();
-    }
-
-    static void toggleFrameLimiter(GamePanel panel) {
-        panel.logEvent(OptionsBoundaryAuthority.toggleFrameLimiter(panel.options));
-        DisplayScaleOptionsSubsystem.applyOptions(panel);
-        panel.graphicsDropdown = -1;
-        panel.repaint();
-    }
-
-    static void setRenderQualityIndex(GamePanel panel, int idx) {
-        panel.logEvent(OptionsBoundaryAuthority.setRenderQualityIndex(panel.options, idx));
-        DisplayScaleOptionsSubsystem.applyOptions(panel);
-        panel.graphicsDropdown = -1;
-        panel.repaint();
-    }
-
-    static void toggleRenderStressTest(GamePanel panel) {
-        panel.logEvent(panel.renderStressTest.toggle(panel.frameLimiter));
-        panel.options.diagnosticsOverlay = true;
-        panel.performanceDiagnostics.setVisible(true);
-        panel.options.save();
-        panel.repaint();
-    }
+    static void cycleLightingFx(GamePanel panel) { DisplayPerformanceOptionsRuntime.cycleLightingFx(panel); }
+    static void toggleReducedMotion(GamePanel panel) { DisplayPerformanceOptionsRuntime.toggleReducedMotion(panel); }
+    static void toggleFrameLimiter(GamePanel panel) { DisplayPerformanceOptionsRuntime.toggleFrameLimiter(panel); }
+    static void setRenderQualityIndex(GamePanel panel, int idx) { DisplayPerformanceOptionsRuntime.setRenderQualityIndex(panel, idx); }
+    static void toggleRenderStressTest(GamePanel panel) { DisplayPerformanceOptionsRuntime.toggleRenderStressTest(panel); }
 }
