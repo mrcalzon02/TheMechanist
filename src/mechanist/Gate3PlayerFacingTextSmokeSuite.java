e4b0e90d63d69c4fcc0aff86b7517319fa0c5ea7
mrcalzon02 <mrcalzon02@yahoo.com>
2026-07-15 22:29:50 -0800

    Add blueprint contract reward smoke to Gate 3

--- src/mechanist/Gate3PlayerFacingTextSmokeSuite.java
+++ src/mechanist/Gate3PlayerFacingTextSmokeSuite.java
@@ -141,6 +141,7 @@ final class Gate3PlayerFacingTextSmokeSuite {
         Milestone05FactionPhysicalConstructionSmoke.main(args);
         Milestone05FactionRoomShellConstructionSmoke.main(args);
         Milestone05FactionStrategicAssetSmoke.main(args);
+        Milestone05BlueprintContractRewardSmoke.main(args);
         Milestone05RoomOwnershipCommandSmoke.main(args);
         Milestone05RoomOwnershipUiSmoke.main(args);
         Milestone05RoomPurchaseSmoke.main(args);
