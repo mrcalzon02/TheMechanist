104ff26ea263d331e7533a6a125a5f440346f1c5
mrcalzon02 <mrcalzon02@yahoo.com>
2026-07-20 10:31:02 -0800

    Release contract: require single-transition lobby failure teardown

--- ROOT_build/ci/verify_synthetic_release_contract.py
+++ ROOT_build/ci/verify_synthetic_release_contract.py
@@ -69,6 +69,7 @@ REQUIRED_TRUE = (
     "independentHostRemoteLobbyEditableConnection",
     "independentHostRemoteLobbyMutableStorageOutsideInstall",
     "independentHostRemoteLobbyPendingConnectionCancellable",
+    "independentHostRemoteLobbyFailedSessionTeardown",
     "independentHostUnsupportedWorldCommandsRejected",
     "independentHostRelayOnlyAccess",
     "independentHostPreAuthenticationDataDenied",
